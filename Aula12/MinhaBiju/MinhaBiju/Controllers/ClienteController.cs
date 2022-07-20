e12efcc5742f8e3116c2f45845bb1d82deebafb5
Jonatas Afonso <jonatas_afonso@hotmail.com>
2022-07-20 21:28:51 +0100

    diferetneeete

---
+++ Aula12/MinhaBiju/MinhaBiju/Controllers/ClienteController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MinhaBiju.Models;
+
+namespace MinhaBiju.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult ListagemDeClientes()
+        {
+            return View();
+        }
+
+        public IActionResult ListagemDeClientesPassandoDadosDoDB()
+        {
+            var listaCliente = new List<Cliente>();
+            listaCliente.Add(new Cliente { Nome = "Fulano", DataNascimento = new DateTime(2000, 01, 01) });
+            listaCliente.Add(new Cliente { Nome = "Beltrano", DataNascimento = new DateTime(2000, 01, 01) });
+            return View(listaCliente);
+        }
+
+
+        public IActionResult CriarNovoCliente()
+        {
+            return View();
+        }
+    }
+}
